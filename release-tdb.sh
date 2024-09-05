4e417cb6734a4f81bd18f590941ad9fd3418c851
ModoX <moardox@gmail.com>
2024-09-05 22:14:34 +0200

    Added --hex-blob to mysqldump calls
    (cherry picked from commit 400df5e6eb9564d437427ad35d12dfada4d25fe2)

--- release-tdb.sh
+++ release-tdb.sh
@@ -165,12 +165,12 @@ mysql -uroot -D hotfixes -e "update \`updates\` set \`state\`='ARCHIVED',\`speed
 # mysql -uroot -D characters -e "update \`worldstates\` set \`value\`=0;"
 
 # 13. update base dbs sql
-mysqldump -uroot auth --default-character-set='utf8mb4' --routines --result-file sql/base/auth_database.sql
+mysqldump -uroot auth --default-character-set='utf8mb4' --hex-blob --routines --result-file sql/base/auth_database.sql
 sed -i -e 's$VALUES ($VALUES\n($g' sql/base/auth_database.sql
 sed -i -e 's$),($),\n($g' sql/base/auth_database.sql
 sed -i -e 's/DEFINER=[^*]*\*/\*/' sql/base/auth_database.sql
 # sed -i -e 's/utf8mb4_0900_ai_ci/utf8mb4_unicode_ci/g' sql/base/auth_database.sql
-mysqldump -uroot characters --default-character-set='utf8mb4' --routines --result-file sql/base/characters_database.sql
+mysqldump -uroot characters --default-character-set='utf8mb4' --hex-blob --routines --result-file sql/base/characters_database.sql
 sed -i -e 's$VALUES ($VALUES\n($g' sql/base/characters_database.sql
 sed -i -e 's$),($),\n($g' sql/base/characters_database.sql
 sed -i -e 's/DEFINER=[^*]*\*/\*/' sql/base/characters_database.sql
@@ -186,8 +186,8 @@ git add sql
 # 14. dump world db to sql
 mkdir tdb
 cd tdb
-mysqldump -uroot world --default-character-set='utf8mb4' --routines --result-file $NEW_TDB_WORLD_FILE.sql
-mysqldump -uroot hotfixes --default-character-set='utf8mb4' --routines --result-file $NEW_TDB_HOTFIXES_FILE.sql
+mysqldump -uroot world --default-character-set='utf8mb4' --hex-blob --routines --result-file $NEW_TDB_WORLD_FILE.sql
+mysqldump -uroot hotfixes --default-character-set='utf8mb4' --hex-blob --routines --result-file $NEW_TDB_HOTFIXES_FILE.sql
 sed -i -e 's/DEFINER=[^*]*\*/\*/' $NEW_TDB_WORLD_FILE.sql
 sed -i -e 's/DEFINER=[^*]*\*/\*/' $NEW_TDB_HOTFIXES_FILE.sql
 
