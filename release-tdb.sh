db6b82d617e83879d46560df52a7fab284cf478f
Shauren <shauren.trinity@gmail.com>
2024-09-26 19:58:09 +0200

    Don't strip SQL SECURITY from CREATE VIEW

--- release-tdb.sh
+++ release-tdb.sh
@@ -168,18 +168,18 @@ mysql -uroot -D hotfixes -e "update \`updates\` set \`state\`='ARCHIVED',\`speed
 mysqldump -uroot auth --default-character-set='utf8mb4' --hex-blob --routines --result-file sql/base/auth_database.sql
 sed -i -e 's$VALUES ($VALUES\n($g' sql/base/auth_database.sql
 sed -i -e 's$),($),\n($g' sql/base/auth_database.sql
-sed -i -e 's/DEFINER=[^*]*\*/\*/' sql/base/auth_database.sql
+sed -i -e 's/DEFINER=.* \(SQL SECURITY\)/\1/' sql/base/auth_database.sql
 # sed -i -e 's/utf8mb4_0900_ai_ci/utf8mb4_unicode_ci/g' sql/base/auth_database.sql
 mysqldump -uroot characters --default-character-set='utf8mb4' --hex-blob --routines --result-file sql/base/characters_database.sql
 sed -i -e 's$VALUES ($VALUES\n($g' sql/base/characters_database.sql
 sed -i -e 's$),($),\n($g' sql/base/characters_database.sql
-sed -i -e 's/DEFINER=[^*]*\*/\*/' sql/base/characters_database.sql
+sed -i -e 's/DEFINER=.* \(SQL SECURITY\)/\1/' sql/base/characters_database.sql
 # sed -i -e 's/utf8mb4_0900_ai_ci/utf8mb4_unicode_ci/g' sql/base/auth_database.sql
 mysqldump -uroot world --default-character-set='utf8mb4' --routines --no-data --result-file sql/base/dev/world_database.sql
-sed -i -e 's/DEFINER=[^*]*\*/\*/' sql/base/dev/world_database.sql
+sed -i -e 's/DEFINER=.* \(SQL SECURITY\)/\1/' sql/base/dev/world_database.sql
 # sed -i -e 's/utf8mb4_0900_ai_ci/utf8mb4_unicode_ci/g' sql/base/auth_database.sql
 mysqldump -uroot hotfixes --default-character-set='utf8mb4' --routines --no-data --result-file sql/base/dev/hotfixes_database.sql
-sed -i -e 's/DEFINER=[^*]*\*/\*/' sql/base/dev/hotfixes_database.sql
+sed -i -e 's/DEFINER=.* \(SQL SECURITY\)/\1/' sql/base/dev/hotfixes_database.sql
 # sed -i -e 's/utf8mb4_0900_ai_ci/utf8mb4_unicode_ci/g' sql/base/dev/hotfixes_database.sql
 git add sql
 
