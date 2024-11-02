b5f0861e1163510d91bfbdf0563c1ddc09d3a8ff
funjoker <funjoker109@gmail.com>
2024-11-02 21:21:55 +0100

    Remove wrong spaces

--- release-tdb.sh
+++ release-tdb.sh
@@ -214,8 +214,8 @@ mysql -uroot -D world < tdb/$NEW_TDB_WORLD_FILE.sql
 mysql -uroot -D hotfixes < tdb/$NEW_TDB_HOTFIXES_FILE.sql
 
 # 20. update revision_data.h.in.cmake with new TDB file name
-sed -i -e 's$ #define DATABASE_FULL_DATABASE      "[A-Za-z0-9$_.]*"$ #define DATABASE_FULL_DATABASE      "'$NEW_TDB_WORLD_FILE'.sql"$g' revision_data.h.in.cmake
-sed -i -e 's$ #define DATABASE_HOTFIXES_DATABASE  "[A-Za-z0-9$_.]*"$ #define DATABASE_HOTFIXES_DATABASE  "'$NEW_TDB_HOTFIXES_FILE'.sql"$g' revision_data.h.in.cmake
+sed -i -e 's$#define DATABASE_FULL_DATABASE      "[A-Za-z0-9$_.]*"$#define DATABASE_FULL_DATABASE      "'$NEW_TDB_WORLD_FILE'.sql"$g' revision_data.h.in.cmake
+sed -i -e 's$#define DATABASE_HOTFIXES_DATABASE  "[A-Za-z0-9$_.]*"$#define DATABASE_HOTFIXES_DATABASE  "'$NEW_TDB_HOTFIXES_FILE'.sql"$g' revision_data.h.in.cmake
 git add revision_data.h.in.cmake
 # 21. commit and push
 git commit -m "$NEW_TDB_NAME - "`date +%Y/%m/%d`
